000d5800727963b2e5690f7f1b7174ea1505009b
Tausif1309 <98589969+Tausif1309@users.noreply.github.com>
2026-09-04 13:16:38 +0530

    Time: 1 ms (99.79%) | Memory: 45.9 MB (97.45%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+        int n = nums.length;
+
+        int[] rightMin = new int[n];
+
+        rightMin[n - 1] = nums[n - 1];
+
+        // Minimum from i to the right
+        for (int i = n - 2; i >= 0; i--) {
+            rightMin[i] = Math.min(nums[i], rightMin[i + 1]);
+        }
+
+        int leftMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            leftMax = Math.max(leftMax, nums[i]);
+
+            if (leftMax - rightMin[i] <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
