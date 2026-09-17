2b70c1b50818679fecefdfce5b5240f2d8b14fb3
Tausif1309 <98589969+Tausif1309@users.noreply.github.com>
2026-09-17 19:28:56 +0530

    Time: 1 ms (99.95%) | Memory: 46.4 MB (68.01%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] alphabet = new int[26];
+        for (char c : magazine.toCharArray()) {
+            alphabet[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            alphabet[c - 'a']--;
+
+            if (alphabet[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
