7632bf962de6855f872050e3d9038e0e65dbe810
Tausif1309 <98589969+Tausif1309@users.noreply.github.com>
2026-09-20 20:32:11 +0530

    Time: 1 ms (99.86%) | Memory: 43.2 MB (55.31%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestPalindrome(String s) {
+
+        int[] freq = new int[128];
+
+        for (char c : s.toCharArray()) {
+            freq[c]++;
+        }
+
+        int ans = 0;
+        boolean hasOdd = false;
+
+        for (int count : freq) {
+
+            if (count % 2 == 0) {
+                ans += count;
+            } else {
+                ans += count - 1;
+                hasOdd = true;
+            }
+        }
+
+        if(hasOdd){
+            ans++;
+        }
+        return ans;
+    }
+}
