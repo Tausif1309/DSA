fe1fa000357d5b83fd8bcb26f1886b0d228beb39
Tausif1309 <98589969+Tausif1309@users.noreply.github.com>
2026-09-21 22:41:20 +0530

    Time: 1 ms (89.18%) | Memory: 43.1 MB (61.74%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count=0;
+
+        for(char c:word.toCharArray()){
+            if(c <= 90){
+                count++;
+            }
+        }
+
+        if(count==0){
+            return true;
+        }
+        else if(count==word.length()){
+            return true;
+        }
+        else if(count==1 && Character.isUpperCase(word.charAt(0))){
+            return true;
+        }
+
+        return false;
+    }
+}
