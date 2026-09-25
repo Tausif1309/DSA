20330e90e0d4223111baf1698a90c2eab0da2b6f
Tausif1309 <98589969+Tausif1309@users.noreply.github.com>
2026-09-25 11:13:45 +0530

    Time: 44 ms (90.51%) | Memory: 79.1 MB (65.28%) - LeetSync

---
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxDistance(int[] position, int m) {
+
+        Arrays.sort(position);
+
+        int low = 1;
+        int high = position[position.length - 1] - position[0];
+
+        int ans = 0;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (works(position, m, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private boolean works(int[] position, int m, int distance) {
+
+        int count = 1;
+        int lastPosition = position[0];
+
+        for (int i = 1; i < position.length; i++) {
+
+            if (position[i] - lastPosition >= distance) {
+                count++;
+                lastPosition = position[i];
+
+                if (count == m) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
